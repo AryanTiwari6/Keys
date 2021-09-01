3b6a11d7710708066848d24d6a68c68e16f8eaaf
AryanTiwari6 <85170048+AryanTiwari6@users.noreply.github.com>
2021-09-02 00:25:10 +0530

    debugged

--- Keys/script.js
+++ Keys/script.js
@@ -9,8 +9,8 @@ function submitform() {
 
     var x = document.getElementById("note");
     var flag = false;
-    for (int i = 0, i < 4, i++) {
-        for (int j = i, j < 4, i++) {
+    for (let i = 0; i < 4; i++) {
+        for (let j = i; j < 4; i++) {
             if (x.elements[i].value == "title" && x.elements[j].value == "title") {
                 result = "Please select at least 3 of the 4 given fields";
                 flag = true;

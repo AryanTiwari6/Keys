0c334edaebc1a776b855ee02d5bf13d09ac039cf
AryanTiwari6 <85170048+AryanTiwari6@users.noreply.github.com>
2021-09-01 22:56:52 +0530

    form final

--- Keys/script.js
+++ Keys/script.js
@@ -1,36 +1,40 @@
 const key = ["F", "C", "G", "D", "A", "E", "B"];
 const mode = ["Lydian", "Major", "Mxolydian", "Dorian", "Minor", "Phrygian", "Locrian"];
 switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
 }
 
 
-function submitform(){
-  alert("Sending Json");
-  var x = document.getElementById("note");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-    text += x.elements[i].value + "<br>";
-  }
-  document.getElementById("container").innerHTML=text;
+function submitform() {
+    var note = 0, mode = 0, acci = 0, sign = 0;
+    alert("Sending Json");
+    
+    var x = document.getElementById("note");
+    note = x.elements[0].value - 2;
+    mode = x.elements[1].value;
+    acci = x.elements[2].value - 2;
+    sign = x.elements[3].value;
+
+    var text = note + "<br>" + mode + "<br>" + acci + "<br>" + sign;
+
+    document.getElementById("container").innerHTML = text;
 }

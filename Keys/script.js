0e3ca9b28104bba15fbacc75e5aa4629c225f21e
AryanTiwari6 <85170048+AryanTiwari6@users.noreply.github.com>
2021-09-02 00:20:32 +0530

    invalid input msg added

--- Keys/script.js
+++ Keys/script.js
@@ -7,31 +7,44 @@ function submitform() {
     var k = 0, m = 0, a = 0, s = 0;
 
     var x = document.getElementById("note");
-    k = x.elements[0].value - 2;
-    a_index = x.elements[1].value - 2;
-    m = x.elements[2].value - 2;
-    s = x.elements[3].value - 7;
-    a = a_index*7;
-
-    result = "";
-
-    if (x.elements[3].value == "title") {
-        s = k+a-m;
-        result = "The key Signature of " + key[k+1] + acci_sym[a_index+1] + " " + mode[m+1] + " is " + Math.abs(s) + " " + acci[Math.sign(s)+1];
-    }
-    if (x.elements[2].value == "title") {
-        m = k+a-s;
-        result = "This key is in the mode of " + mode[m+1];
-    }
-    if (x.elements[0].value == "title") {
-        k = s+m-a;
-        result = "This is the key of " + key[k+1];
-    }
-    if (x.elements[1].value == "title") {
-        a = s+m-k;
-        result = "It's " + acci[((a/7)+1)];
+    var flag = false;
+    for (int i = 0, i < 4, i++) {
+        for (int j = i, j < 4, i++) {
+            if (x.elements[i].value == "title" && x.elements[j].value == "title") {
+                result = "Please select at least 3 of the 4 given fields";
+                flag = true;
+                break;
+            }
+        }
     }
 
+
+    if (flag) {
+        k = x.elements[0].value - 2;
+        a_index = x.elements[1].value - 2;
+        m = x.elements[2].value - 2;
+        s = x.elements[3].value - 7;
+        a = a_index * 7;
+
+        result = "";
+
+        if (x.elements[3].value == "title") {
+            s = k + a - m;
+            result = "The key Signature of " + key[k + 1] + acci_sym[a_index + 1] + " " + mode[m + 1] + " is " + Math.abs(s) + " " + acci[Math.sign(s) + 1];
+        }
+        if (x.elements[2].value == "title") {
+            m = k + a - s;
+            result = "This key is in the mode of " + mode[m + 1];
+        }
+        if (x.elements[0].value == "title") {
+            k = s + m - a;
+            result = "This is the key of " + key[k + 1];
+        }
+        if (x.elements[1].value == "title") {
+            a = s + m - k;
+            result = "It's " + acci[((a / 7) + 1)];
+        }
+    }
     document.getElementById("result").innerHTML = result;
 
 }

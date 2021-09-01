89f384b15ca5d46b58099989388d021bc0182a3f
AryanTiwari6 <85170048+AryanTiwari6@users.noreply.github.com>
2021-09-02 00:11:39 +0530

    done

--- Keys/script.js
+++ Keys/script.js
@@ -1,41 +1,37 @@
 const key = ["F", "C", "G", "D", "A", "E", "B"];
 const mode = ["Lydian", "Major", "Mxolydian", "Dorian", "Minor", "Phrygian", "Locrian"];
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-}
-
+const acci = ["flat", "natural", "sharp"];
+const acci_sym = ["b", "", "#"];
 
 function submitform() {
-    var note = 0, mode = 0, acci = 0, sign = 0;
-    alert("Sending Json");
+    var k = 0, m = 0, a = 0, s = 0;
 
     var x = document.getElementById("note");
-    note = x.elements[0].value - 2;
-    acci = x.elements[1].value - 2;
-    mode = x.elements[2].value;
-    sign = x.elements[3].value;
+    k = x.elements[0].value - 2;
+    a_index = x.elements[1].value - 2;
+    m = x.elements[2].value - 2;
+    s = x.elements[3].value - 7;
+    a = a_index*7;
 
-    alert("Sending Json");
+    result = "";
 
-    document.getElementById("result").innerHTML = sign;
+    if (x.elements[3].value == "title") {
+        s = k+a-m;
+        result = "The key Signature of " + key[k+1] + acci_sym[a_index+1] + " " + mode[m+1] + " is " + Math.abs(s) + " " + acci[Math.sign(s)+1];
+    }
+    if (x.elements[2].value == "title") {
+        m = k+a-s;
+        result = "This key is in the mode of " + mode[m+1];
+    }
+    if (x.elements[0].value == "title") {
+        k = s+m-a;
+        result = "This is the key of " + key[k+1];
+    }
+    if (x.elements[1].value == "title") {
+        a = s+m-k;
+        result = "It's " + acci[((a/7)+1)];
+    }
+
+    document.getElementById("result").innerHTML = result;
 
 }

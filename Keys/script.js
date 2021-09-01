eb22929520358ae664452ade93a124e1832a5b32
AryanTiwari6 <85170048+AryanTiwari6@users.noreply.github.com>
2021-09-01 19:47:38 +0530

    readded form

---
+++ Keys/script.js
@@ -0,0 +1,36 @@
+const key = ["F", "C", "G", "D", "A", "E", "B"];
+const mode = ["Lydian", "Major", "Mxolydian", "Dorian", "Minor", "Phrygian", "Locrian"];
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+
+
+function submitform(){
+  alert("Sending Json");
+  var x = document.getElementById("note");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("container").innerHTML=text;
+}
